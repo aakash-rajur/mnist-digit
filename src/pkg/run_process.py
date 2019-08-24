13691c51b7af3b5a0d689bdf4c025ecd601b3e10
aakashRajur <aakashrajur16@gmail.com>
2019-08-24 21:36:00 +0530

    gracefully handle keyboard interrupt

---
+++ src/pkg/run_process.py
@@ -0,0 +1,16 @@
+import sys
+from typing import Callable, Any
+
+
+def run_process(function: Callable[[], Any], cleanup: Callable[[], Any] = None):
+    try:
+        function()
+    except (KeyboardInterrupt, SystemExit):
+        if cleanup is not None:
+            print("Process Interrupted, Cleaning Up!")
+            cleanup()
+        else:
+            print("Process Interrupted, Exiting!")
+            sys.exit(0)
+    else:
+        raise
